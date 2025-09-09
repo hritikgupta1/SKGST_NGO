1ceee077bab1d393632b4cb20c2038fbb90efe6a
Hritik Gupta <hrithikgupta76@gmail.com>
2025-09-09 22:04:42 +0530

    updated files

--- send_mail.php
+++ send_mail.php
@@ -1,5 +1,5 @@
 <?php
-
+// in local host use below code for sending mails:
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -17,15 +17,61 @@ function sendOTP($toEmail, $toName, $otp)
         $mail->SMTPSecure = "tls";
         $mail->Port = 587;
 
+        // Sender details
         $mail->setFrom("dubaispace1234@gmail.com", "Dubai Space");
+
+        // Recipient
         $mail->addAddress($toEmail, $toName);
+
+        // Add CC & BCC
+        // $mail->addCC("acpitzone@gmail.com");
+        // $mail->addBCC("praveshgulati@gmail.com");
+
+        // Email content
         $mail->isHTML(true);
         $mail->Subject = "Your OTP Verification Code";
         $mail->Body = "<h3>Your OTP is: <b>$otp</b></h3><p>Valid for 5 minutes.</p>";
 
+        // Send email
         $mail->send();
         return true;
+
     } catch (Exception $e) {
         return false;
     }
 }
+
+
+// in cpanel use below code for sending mails
+
+// function sendOTP($toEmail, $toName, $otp)
+// {
+//     $subject = "Your OTP Verification Code";
+
+//     $message = "
+//     <html>
+//     <head>
+//         <title>Dubai Space - OTP</title>
+//     </head>
+//     <body>
+//         <h3>Hi $toName,</h3>
+//         <p>Your OTP is: <b>$otp</b></p>
+//         <p>It is valid for <b>5 minutes</b>.</p>
+//     </body>
+//     </html>";
+
+//     // Email headers
+//     $headers  = "MIME-Version: 1.0\r\n";
+//     $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+//     $headers .= "From: Dubai Space <noreply@skgst.in>\r\n";
+//     // $headers .= "Cc: acpitzone@gmail.com\r\n";
+//     // $headers .= "Bcc: praveshgulati@gmail.com\r\n";
+
+//     // Send email using PHP mail()
+//     if (mail($toEmail, $subject, $message, $headers)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+?>
